6cec44333a3c244ea7ede08ec9e4ef1ce019060a
Sollution <egor.odnokoz007307@gmail.com>
2023-04-16 10:40:43 +0300

    Update Statistics.js

--- src/components/statistics/Statistics.js
+++ src/components/statistics/Statistics.js
@@ -9,9 +9,6 @@ export const Statistics = ({
   return (
     <div>
       <div>
-        <h1>Please leave feedback</h1>
-      </div>
-      <div>
         <h2>Statistics</h2>
         <ul>
           <li>Good: {good}</li>
