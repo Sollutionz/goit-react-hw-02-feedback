b1fd465ed05c46a4d276234107610e121383c840
Sollution <egor.odnokoz007307@gmail.com>
2023-04-19 23:30:46 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,7 @@ import { ContactForm } from "./contactForm/ContactForm";
 export class App extends React.Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-1', name: 'Rosie Simpsons', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
